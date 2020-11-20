b078ba6c68adff5f05b68be205a9e349c55ae231
Felix Lehner <f.lehner@tu-bs.de>
2020-11-20 17:37:14 +0100

    on my way to make this installable..

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,6 +1,8 @@
 #include <KatherineTimepix3.h>
-#include <RadiationAngleReconstructor.h>
+#include <GeometryTypes.h>
+//#include <RadiationAngleReconstructor.h>
 #include <string>
+#undef ssize_t
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
 
@@ -15,9 +17,43 @@ PYBIND11_MODULE(Tpx3Angles, m) {
         .def("disconnect", &KatherineTimepix3::disconnect)
         .def("getReadoutDevice", &KatherineTimepix3::getReadoutDevice)
         .def("getTemperature", &KatherineTimepix3::getTemperature)
-        .def(py::self == py::self)
+ //       .def(py::self == py::self)
         .def("__repr__",
             [](KatherineTimepix3& a) {
                 return "<KatherineTimepix3 chipID: " + a.getChipID() + ">";
             });
+    
+    py::class_<FFilterSetup::FDetector>(m, "FDetector")
+        .def(py::init())
+        .def_readwrite("widthIn_mm", &FFilterSetup::FDetector::widthIn_mm)
+        .def_readwrite("heightIn_mm", &FFilterSetup::FDetector::heightIn_mm)
+        .def_readwrite("widthIn_px", &FFilterSetup::FDetector::widthIn_px)
+        .def_readwrite("heightIn_px", &FFilterSetup::FDetector::heightIn_px)
+//        .def_readwrite("pixelPer_mm_x", &FFilterSetup::FDetector::pixelPer_mm)
+        .def("__repr__",
+            [](FFilterSetup::FDetector& a) {
+                return "<FFilterSetup::FDetector>";
+            });
+
+    py::class_<FFilter>(m, "FFilter")
+        .def(py::init())
+        .def("__repr__",
+            [](FFilterSetup::FDetector& a) {
+                return "<FFilterSetup::FDetector>";
+            });
+   
+    py::class_<FFilterSetup>(m, "FFilterSetup")
+        .def(py::init<FFilterSetup::FDetector, std::vector<FFilter>>())
+        .def("__repr__",
+            [](FFilterSetup& a) {
+                return "<FFilterSetup>";
+            });
+    /*
+    py::class_<RadiationAngleReconstructor>(m, "RadiationAngleReconstructor")
+        .def(py::init())
+        .def_static("SetFilterSetup", &RadiationAngleReconstructor::SetFilterSetup)
+        .def("__repr__",
+            [](RadiationAngleReconstructor& a) {
+                return "<RadiationAngleReconstructor " + std::to_string((int)&a) + ">";
+            });*/
 }
