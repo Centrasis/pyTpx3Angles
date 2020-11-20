5e7b1515fdc0eb64fb6f3b5ad110919db4c48464
Felix Lehner <f.lehner@tu-bs.de>
2020-11-20 15:51:32 +0100

    updated tpx dosi

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,4 +1,6 @@
 #include <KatherineTimepix3.h>
+#include <RadiationAngleReconstructor.h>
+#include <string>
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
 
@@ -13,5 +15,9 @@ PYBIND11_MODULE(Tpx3Angles, m) {
         .def("disconnect", &KatherineTimepix3::disconnect)
         .def("getReadoutDevice", &KatherineTimepix3::getReadoutDevice)
         .def("getTemperature", &KatherineTimepix3::getTemperature)
-        .def(py::self == py::self);
+        .def(py::self == py::self)
+        .def("__repr__",
+            [](KatherineTimepix3& a) {
+                return "<KatherineTimepix3 chipID: " + a.getChipID() + ">";
+            });
 }
